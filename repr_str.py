cee163744f3ea3a24701027919112019b9a52376
jy03078584 <xiangmz0928@gmali.com>
2020-08-28 13:55:37 +0800

    improve

--- repr_str.py
+++ repr_str.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+
 class Student:
     def __init__(self, name, age):
         self._name = name
         self._age = age
 
     def __repr__(self):
-        return f'__repr : name:{self._name} ===> age:{self._age}'
+        return f'__repr__ : name:{self._name} ===> age:{self._age}'
 
     def __str__(self):
-        return f'__str: name:{self._name} ===> age:{self._age}'
+        # return self.__repr__()
+        return f'__str__: name:{self._name} ===> age:{self._age}'
 
 
 if __name__ == '__main__':
     student = Student('zhangsan', 20)
-    # 未定义__str__时使用__repr
+    # 未定义__str__时使用__repr__
     print(student)
+    # iterable时 使用__repr__
+    print([student, Student('sisi', 22)])
